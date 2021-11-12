c2287dd34c15d4b4419a95f7bfac17e2df7a5ad1
pjp0704 <junhyeong25@gmail.com>
2021-11-12 23:49:33 +0900

    [ADD] TypeORM connection, bootstrap function

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,30 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 
-const app = express();
+async function bootstrap() {
+  const app = express();
 
-app.use(cors());
-app.use(morgan('combined'));
+  await createConnection({
+    type: 'mysql',
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT!,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities: [User],
+    synchronize: true,
+    logging: false,
+  });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
+  app.use(cors());
+  app.use(morgan('combined'));
+  app.use(express.json());
+
+  app.use(router());
+
+  app.listen(process.env.PORT, () => {
+    console.log('Server listening on port ' + process.env.PORT);
+  });
+}
+
+bootstrap();
